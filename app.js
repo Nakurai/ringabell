5b7bd70ca02f2350c653fec61ee01de9a479fc66
Zuwa Omigie <zuwaomigie@ca.ibm.com>
2016-09-07 20:06:09 -0400

    timer

--- app.js
+++ app.js
@@ -2,12 +2,19 @@
 var ringabell = require('./ringabell.js');
 
 class App{
+  // const TESTTIME = 100000;
 
   /**
   Define all useful variables for this Class
   */
   constructor() {
     this.bell = ringabell.ringabell;
+    this.currentTime = new Date();
+
+    this.TEST_TIME = 3000;
+    this.HALF_HOUR = 1800000;
+    this.HOUR = 3600000;
+
     this.getMinutesBeforeNext();
   }
 
@@ -18,18 +25,29 @@ class App{
    * @return {[type]} [description]
    */
   getMinutesBeforeNext() {
-    var now = new Date().now();
-    
-    now.setHours(now.getHours()+1);
-
-    if(now.getMinutes()>30){
-
-    }
-    console.log("test");
+    // 30 mins to 1800 000 milleseconds
+    // 1 hour 3600 000 milleseconds
+    setTimeout( this.eventHandler, this.TEST_TIME);
   }
 
-  triggerBell(){
+  eventHandler() {
+    var n = 1;
+    if (this.currentTime.getMinutes() === 0) {
+      n = this.currentTime.getHours();
+    }
 
+    this.triggerBell(n);
+  }
+
+  /**
+   * Ring the bell
+   * @param  {Number} n number of times to ring bell
+   *
+   */
+  triggerBell(n){
+    // simulate bell ring
+    console.log("BELL RING");
+    ringabell.ring(n);
   }
 
 }
